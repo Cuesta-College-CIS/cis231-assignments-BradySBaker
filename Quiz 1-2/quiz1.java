65b97aa2f1ea50b3296253e57b367b68f04aee49
Brady Baker <bradysamuelbaker@gmail.com>
2022-04-03 17:18:40 -0700

    Create File

---
+++ Quiz 1-2/quiz1.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class quiz1 {
+	public static void main(String[] args) {
+		final int CNT = 5;
+		int minval, minidx, tmp;
+
+		Scanner scnr = new Scanner(System.in);
+		int N = scnr.nextInt();
+		int[] numbers = new int[N];
+		for (int i = 0; i < N; i++)
+			numbers[i] = scnr.nextInt();
+
+		// Your code
+
+		scnr.close();
+	}
+
+}
