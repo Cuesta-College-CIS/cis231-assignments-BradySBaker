c2ea54ee014046547c09053b27ee26a0065a9fd6
Brady Baker <bradysamuelbaker@gmail.com>
2022-04-03 20:21:11 -0700

    Quiz 2 Created

---
+++ Quiz 1-2/quiz2.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class quiz2 {
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        final int L = 3; // will NOT use L. Just for the extendability.
+        final int M = 3;
+        final int N = 2;
+        int[][] matrix1 = { { 2, 5, 10 },
+                { 3, 6, 9 },
+                { 1, 2, 3 } };
+        int[][] matrix2 = { { 10, 20 },
+                { 30, 40 },
+                { 50, 60 } };
+        int rowidx = scnr.nextInt();
+
+        // Your code
+
+    }
+
+}
