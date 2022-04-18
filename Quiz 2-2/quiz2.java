3f38d0f7aa8dd0988ae8f4d9c4a96890697e111a
Brady Baker <bradysamuelbaker@gmail.com>
2022-04-17 20:45:59 -0700

    First function working

--- Quiz 2-2/quiz2.java
+++ Quiz 2-2/quiz2.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Random;
+
 public class quiz2 {
 
+    public static int[] constructArray(Scanner scnr) {
+
+        Random rand = new Random();
+        int[] tempArray = new int[scnr.nextInt()];
+        for (int i = 0; i < tempArray.length; i++) {
+            tempArray[i] = rand.nextInt(99);
+        }
+        return tempArray;
+    }
+
+    public static void main(String[] args) {
+        Scanner scnr = new Scanner(System.in);
+        int[] array1;
+        array1 = constructArray(scnr);
+        System.out.println(Arrays.toString(array1));
+    }
 }
