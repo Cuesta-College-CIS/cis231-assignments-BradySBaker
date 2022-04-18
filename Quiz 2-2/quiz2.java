f9b8c2ab4c5cd80b2c8ee75ebb316a9449338c5a
Brady Baker <bradysamuelbaker@gmail.com>
2022-04-17 21:31:18 -0700

    Method 3 working

--- Quiz 2-2/quiz2.java
+++ Quiz 2-2/quiz2.java
@@ -18,6 +18,26 @@ public class quiz2 {
         System.out.println(Arrays.toString(printArray));
     }
 
+    public static int findMoreEvens(int[] n1, int[] n2) {
+        int highestValue = 0;
+        int n1Value = 0;
+        int n2Value = 0;
+        for (int i = 0; i < n1.length; i++) {
+            if (n1[i] % 2 == 0) {
+                n1Value = n1Value + 1;
+            }
+            if (n2[i] % 2 == 0) {
+                n2Value = n2Value + 1;
+            }
+        }
+        if (n1Value > n2Value) {
+            highestValue = 1;
+        } else {
+            highestValue = 2;
+        }
+        return highestValue;
+    }
+
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         int[] array1;
@@ -27,5 +47,7 @@ public class quiz2 {
         array2 = constructArray(arrayLength);
         printArray(array1);
         printArray(array2);
+        int highestValue = findMoreEvens(array1, array2);
+        System.out.println("Array " + highestValue + " has more even numbers");
     }
 }
