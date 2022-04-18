fdbdd464c0e4ab84c54960addda71f64868cbd53
Brady Baker <bradysamuelbaker@gmail.com>
2022-04-17 21:09:56 -0700

    Fixed issues finished method 2

--- Quiz 2-2/quiz2.java
+++ Quiz 2-2/quiz2.java
@@ -4,20 +4,28 @@ import java.util.Random;
 
 public class quiz2 {
 
-    public static int[] constructArray(Scanner scnr) {
+    public static int[] constructArray(int length) {
 
         Random rand = new Random();
-        int[] tempArray = new int[scnr.nextInt()];
+        int[] tempArray = new int[length];
         for (int i = 0; i < tempArray.length; i++) {
-            tempArray[i] = rand.nextInt(99);
+            tempArray[i] = rand.nextInt(100);
         }
         return tempArray;
+    }
+
+    public static void printArray(int[] printArray) {
+        System.out.println(Arrays.toString(printArray));
     }
 
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
         int[] array1;
-        array1 = constructArray(scnr);
-        System.out.println(Arrays.toString(array1));
+        int[] array2;
+        int arrayLength = scnr.nextInt();
+        array1 = constructArray(arrayLength);
+        array2 = constructArray(arrayLength);
+        printArray(array1);
+        printArray(array2);
     }
 }
