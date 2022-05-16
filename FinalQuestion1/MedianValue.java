0e22b3961e020fb6dc37bbea4c8d362e39cd93be
Brady Baker <bradysamuelbaker@gmail.com>
2022-05-15 17:58:25 -0700

    Array Created

--- FinalQuestion1/MedianValue.java
+++ FinalQuestion1/MedianValue.java
@@ -1,9 +1,16 @@
 package FinalQuestion1;
 
+import java.util.Scanner;
 import java.util.Arrays;
 
 public class MedianValue {
     public static void main(String[] args) {
-
+        Scanner scnr = new Scanner(System.in);
+        int n = scnr.nextInt();
+        int inputArray[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            inputArray[i] = (int) Math.floor(Math.random() * 100);
+        }
+        System.out.println(Arrays.toString(inputArray));
     }
 }
