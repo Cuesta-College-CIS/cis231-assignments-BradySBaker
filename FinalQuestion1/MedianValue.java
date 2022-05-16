388b88a00371afac3af6cf7d42ec63dc6c9fd483
Brady Baker <bradysamuelbaker@gmail.com>
2022-05-15 18:05:42 -0700

    Working Program

--- FinalQuestion1/MedianValue.java
+++ FinalQuestion1/MedianValue.java
@@ -8,9 +8,15 @@ public class MedianValue {
         Scanner scnr = new Scanner(System.in);
         int n = scnr.nextInt();
         int inputArray[] = new int[n];
-        for (int i = 0; i < n; i++) {
+
+        for (int i = 0; i < n; i++) { // Fills array with random numbers
             inputArray[i] = (int) Math.floor(Math.random() * 100);
         }
-        System.out.println(Arrays.toString(inputArray));
+        System.out.println("Orignal List: " + Arrays.toString(inputArray));
+        Arrays.sort(inputArray); // Sort Array
+        System.out.println("Sorted List: " + Arrays.toString(inputArray));
+        int idxMid = (int) Math.ceil(n / 2); // Find mid value locaiton
+
+        System.out.println("Median Value: " + inputArray[idxMid]); // Print out mid value
     }
 }
